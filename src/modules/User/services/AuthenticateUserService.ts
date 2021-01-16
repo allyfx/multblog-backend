8ffcabd388403d6d65026be9796eec39c4bfe358
Alicia Foureaux <aliciafoureauxlucas@gmail.com>
2021-01-16 12:33:11 -0300

    Authenticate user

---
+++ src/modules/User/services/AuthenticateUserService.ts
@@ -0,0 +1,49 @@
+import { sign } from 'jsonwebtoken';
+import authConfig from 'config/auth';
+
+import User from 'modules/User/entities/User';
+import HashProvider from '../providers/HashProvider';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+import AppError from 'shared/errors/AppError';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  private hashProvider: IHashProvider;
+
+  constructor () {
+    this.hashProvider = new HashProvider(); 
+  }
+
+  public async execute({ email, password }: IRequest) {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.');
+    }
+
+    const passwordMatch = await this.hashProvider.compareHash(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError('Incorrect email/password combination.');
+    }
+
+    const { expiredIn, secret } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn: expiredIn,
+    });
+
+    return {
+      user,
+      token,
+    }
+  }
+}
+
+export default AuthenticateUserService;
--- src/modules/User/user.routes.ts
+++ src/modules/User/user.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import UsersController from 'modules/User/controllers/UsersController';
+import SessionsController from 'modules/User/controllers/SessionsController';
 
 const usersController = new UsersController();
+const sessionsController = new SessionsController();
 
 const userRouter = Router();
 
 userRouter.post('/create', usersController.create);
+userRouter.post('/authenticate', sessionsController.create);
 
 export default userRouter;
